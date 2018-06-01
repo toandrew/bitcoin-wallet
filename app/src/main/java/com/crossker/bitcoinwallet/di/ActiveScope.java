cb33aa00f49d430fcbc1e973189638b3c621ef69
andrew <jianmin.jim@gmail.com>
2018-06-01 17:11:19 +0800

    Add basic activities
    Change-Id: Ieaa7df5070bee5bcbd2a852e505a48b8639a80fc

---
+++ app/src/main/java/com/crossker/bitcoinwallet/di/ActiveScope.java
@@ -0,0 +1,11 @@
+package com.crossker.bitcoinwallet.di;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActiveScope {
+}
