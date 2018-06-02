8838ba0b3455aff9a986b5c468186556681a951f
andrew <jianmin.jim@gmail.com>
2018-06-02 14:23:03 +0800

    Update splash
    Change-Id: I77e460caa163be354b44d042f30bd5272b2f69a8

--- app/src/main/java/com/crossker/bitcoinwallet/ui/splash/SplashActivity.java
+++ app/src/main/java/com/crossker/bitcoinwallet/ui/splash/SplashActivity.java
@@ -1,25 +1,56 @@
 package com.crossker.bitcoinwallet.ui.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.crossker.bitcoinwallet.R;
 import com.crossker.bitcoinwallet.ui.base.BaseActivity;
+import com.crossker.bitcoinwallet.ui.main.MainActivity;
+
+import butterknife.BindView;
 
 public class SplashActivity extends BaseActivity {
+    private static final String TAG = "SplashActivity";
+
+    @BindView(R.id.sp_jump_btn)
+    Button skipBtn;
+
+    private CountDownTimer timer = new CountDownTimer(3400, 1000) {
+        @Override
+        public void onTick(long l) {
+            skipBtn.setText(l / 1000 + "s");
+        }
+
+        @Override
+        public void onFinish() {
+            skipBtn.setVisibility(View.GONE);
+
+            Intent intent = new Intent();
+            intent.setClass(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+        timer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        timer.cancel();
     }
 
     @Override
